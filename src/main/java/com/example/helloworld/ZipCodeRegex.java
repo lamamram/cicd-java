ac4f7ac08613364ea6328baa85e4487a6c6f061e
mlamamra <mlamamra@dawan.fr>
2024-05-21 16:55:51 +0200

    bootstrap du projet maven

---
+++ src/main/java/com/example/helloworld/ZipCodeRegex.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+import java.util.regex.Pattern;
+
+public class ZipCodeRegex {
+    private Pattern pattern;
+
+    public ZipCodeRegex(String patternString){
+        this.pattern = Pattern.compile((patternString));
+    }
+}
